fd89f2a8f8686d62be00c9fe294bd0dea8a0783d
issxjl2015 <issxjl2015@outlook.com>
2022-04-29 21:29:21 +0800

    Initial commit

---
+++ src/main/java/com/issxjl2015/skrshop/SkrShopApplication.java
@@ -0,0 +1,13 @@
+package com.issxjl2015.skrshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkrShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/issxjl2015/skrshop/SkrShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issxjl2015.skrshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
